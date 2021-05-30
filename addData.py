f7098548002be216a816d63885a2fc1e958b078b
gary88888888 <a88386013@gmail.com>
2021-05-30 10:03:34 +0800

    insert data

---
+++ addData.py
@@ -0,0 +1,23 @@
+import os
+import django
+import requests as rq
+import json
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "datacenter.settings")
+django.setup()
+
+from mysite.models import TrafficAccident
+
+url ="https://api.kcg.gov.tw/api/service/get/f5a4a10e-ccec-4e42-a59e-1eab80560922"
+rawdata = rq.get(url)
+# json_dict = json.loads(resp.text)
+data = rawdata.json()["data"]
+# print(data)
+
+
+for d in data:
+
+    newData = TrafficAccident(yearMonth = d["事故年月"],processingUnit = d["單位名稱"],accidentLevel = d["事故類別"],accidentTime = d["發生日期"],dist = d["鄉鎮市區"],streetRoad = d["街路"],roadclass = d["道路說明"],roadType = d["道路型態說明"],light = d["光線說明"],accidentType = d["事故類型及型態說明"],whether = d["天候說明"],deathNum = d["死亡人數"],injuryNum = d["受傷人數"],roadStatus = d["路面狀態說明"],roadDefect = d["路面缺陷說明"],speedLimit = d["速限"],obstacle = d["障礙物說明"],positionX = d["x"],positionY = d["y"])
+
+    newData.save()
+    # newData = d["事故年月"]
+    # print(newData)
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
+
+class TrafficAccident(models.Model):
+
+    accidentID = models.CharField(max_length=200)#案件編號
+    yearMonth = models.CharField(max_length=200)#事故年月
+    processingUnit = models.CharField(max_length=200)#單位名稱
+    accidentLevel = models.CharField(max_length=200)#事故類別
+    accidentTime = models.CharField(max_length=200)#發生日期
+    dist = models.CharField(max_length=200)#鄉鎮市區
+    streetRoad = models.CharField(max_length=200)#街路
+    roadclass = models.CharField(max_length=200)#道路說明 ex.市區道路
+    roadType = models.CharField(max_length=200)#道路型態說明    ex.直路 or 岔路
+    light = models.CharField(max_length=200)#光線說明    ex.日間自然光線
+    accidentType = models.CharField(max_length=200)#事故類型及型態說明    ex.側撞
+    whether = models.CharField(max_length=200)#天候說明    ex.晴
+    deathNum = models.CharField(max_length=200)#死亡人數    ex.1
+    injuryNum = models.CharField(max_length=200)#受傷人數   ex.1
+    roadStatus = models.CharField(max_length=200)#路面狀態說明    ex.乾燥
+    roadDefect = models.CharField(max_length=200)#路面缺陷說明    ex.無缺陷
+    speedLimit = models.CharField(max_length=200)#速限   ex.50
+    obstacle = models.CharField(max_length=200)#障礙物說明   ex.無障礙物
+    positionX = models.CharField(max_length=200)#x   ex.22.6810731
+    positionY = models.CharField(max_length=200)#y   ex.120.3101755
+    class Meta:
+        ordering = ('-accidentTime',)
+
+    def __str__(self):
+        return self.accidentID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Create your models here.
