6cb5f9b6daca9779d41ecb085fbc8425ef9efa30
gary88888888 <a88386013@gmail.com>
2021-06-08 21:28:25 +0800

    add test.txt

--- addData.py
+++ addData.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import os
 import django
 import requests as rq
@@ -7,17 +8,24 @@ django.setup()
 
 from mysite.models import TrafficAccident
 
-url ="https://api.kcg.gov.tw/api/service/get/d32c8bcf-82b2-4061-92f8-0fe2efc29cd3"
-rawdata = rq.get(url)
-# json_dict = json.loads(resp.text)
-data = rawdata.json()["data"]
-# print(data)
+listURL =[
+    'https://api.kcg.gov.tw/api/service/get/5954c99d-8579-4521-971e-224033c52bfc',#6
+    'https://api.kcg.gov.tw/api/service/get/3e092ef1-9a19-4e52-b567-eef48120147c',#7
+    'https://api.kcg.gov.tw/api/service/get/cd4519a2-a84c-411d-bad0-aee66e2f042d',#9
+    'https://api.kcg.gov.tw/api/service/get/ad15de83-8642-4416-bfd1-be74fe8896e6',#10
+]
+for url in listURL:
+
+    rawdata = rq.get(url)
+
+    data = rawdata.json()["data"]
+    # print(data)
 
 
-for d in data:
+    for d in data:
 
-    newData = TrafficAccident(yearMonth = d["事故年月"],processingUnit = d["單位名稱"],accidentLevel = d["事故類別"],accidentTime = d["發生日期"],dist = d["鄉鎮市區"],streetRoad = d["街路"],roadclass = d["道路說明"],roadType = d["道路型態說明"],light = d["光線說明"],accidentType = d["事故類型及型態說明"],whether = d["天候說明"],deathNum = d["死亡人數"],injuryNum = d["受傷人數"],roadStatus = d["路面狀態說明"],roadDefect = d["路面缺陷說明"],speedLimit = d["速限"],obstacle = d["障礙物說明"],positionX = d["x"],positionY = d["y"])
+        # newData = TrafficAccident(yearMonth = d["事故年月"],processingUnit = d["單位名稱"],accidentLevel = d["事故類別"],accidentTime = d["發生日期"],dist = d["鄉鎮市區"],streetRoad = d["街路"],roadclass = d["道路說明"],roadType = d["道路型態說明"],light = d["光線說明"],accidentType = d["事故類型及型態說明"],whether = d["天候說明"],deathNum = d["死亡人數"],injuryNum = d["受傷人數"],roadStatus = d["路面狀態說明"],roadDefect = d["路面缺陷說明"],speedLimit = d["速限"],obstacle = d["障礙物說明"],positionX = d["x"],positionY = d["y"])
 
-    newData.save()
-    # newData = d["事故年月"]
-    # print(newData)
+        # newData.save()
+        # newData = d["事故年月"]
+        print(d["事故年月"])
