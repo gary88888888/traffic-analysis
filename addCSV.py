adccb689e679da8f6cc5305a975f2463115734ac
gary88888888 <a88386013@gmail.com>
2021-06-20 13:32:39 +0800

    fix5

--- addCSV.py
+++ addCSV.py
@@ -14,4 +14,4 @@ with open(path ,"r") as csvfile:
 	for d in rows:
 		print(d[0], d[2])
 		newdata = SpeedMeasuring(cameraID = d[0],cameraType = d[1],location = d[2],direction = d[3],limit = d[4],dist = d[5],ViolationType = d[6],positionX = d[7],positionY = d[8])
-		# newdata.save()
+		newdata.save()
